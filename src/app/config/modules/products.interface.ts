49101f1daa3a2b22b230910153a0e6280d4e047e
Forhad96 <forhadairdrop@gmail.com>
2024-05-20 23:46:07 -0500

    feat: add TypeScript interface for product structure

---
+++ src/app/config/modules/products.interface.ts
@@ -0,0 +1,20 @@
+
+interface Product {
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  tags: string[];
+  variants: Variant[];
+  inventory: Inventory;
+}
+
+interface Variant {
+  type: string;
+  value: string;
+}
+
+interface Inventory {
+  quantity: number;
+  inStock: boolean;
+}
